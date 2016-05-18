fb79dd6430765363f6c6c40f07ead75831b154a6
Misternutz <pierre.leriche94@gmail.com>
2016-05-18 21:38:21 +0200

    commit alphabet

---
+++ src/Vue/Alphabet.java
@@ -0,0 +1,30 @@
+package testVue;
+
+public class Alphabet {
+	
+	private String[] alphabet;
+	
+	public Alphabet(){
+		alphabet = new String[26];
+		alphabet[1] = "A";
+		alphabet[2] = "B";
+		alphabet[3] = "C";
+		alphabet[4] = "D";
+		alphabet[5] = "E";
+		alphabet[6] = "F";
+		alphabet[7] = "G";
+		alphabet[8] = "H";
+		alphabet[9] = "I";
+		alphabet[10] = "J";
+		
+	}
+
+	public String[] getAlphabet() {
+		return alphabet;
+	}
+
+	public void setAlphabet(String[] alphabet) {
+		this.alphabet = alphabet;
+	}
+
+}
