41f9f14f3f38299ea0b266b5a319cd4795d79323
Misternutz <pierre.leriche94@gmail.com>
2016-05-19 19:41:54 +0200

    CHangement algo aleatoire

--- src/Modele/Plateau.java
+++ src/Modele/Plateau.java
@@ -39,71 +39,75 @@ public class Plateau {
 	}
 
 	public void placerAleatoireBateau(Bateau b) {
-
+		
 		boolean placer = false;
 		boolean cut = false;
 		int orient;
-
+		
 		// Placement horizontal
 		while (!placer) {
-			cut = false;
-			orient = 1 + (int) (Math.random() * ((2 - 1) + 1));
-			if (orient == 1) {
-
-				int ligne = (int) 1 + (int) (Math.random() * ((carte.length - 2) + 1));
-				int colone = 1 + (int) (Math.random() * (((carte.length - b.taille)) + 1));
+		cut = false;
+		orient = 1 + (int) (Math.random() * ((2 - 1) + 1));
+		if (orient == 1) {
+			
+				int ligne = (int) 0 + (int) (Math.random() * ((carte.length -1) + 1));
+				int colone = 0 + (int) (Math.random() * (((carte.length - b.taille)) + 1));
+				System.out.println("ligne :" + ligne + " colone : " + colone);
 				ArrayList<CaseBateau> caseb = new ArrayList<CaseBateau>();
 				for (int p = 0; p < b.taille; p++) {
-					if (colone + p > carte.length - 1) {
+					if(colone+p> carte.length-1){
 						cut = true;
 					}
-
+					
 				}
-
-				if (!cut) {
-					for (int p = 0; p < b.taille; p++) {
-						if (carte[ligne][colone + p] != null) {
-							cut = true;
-						}
-
+				
+				if(!cut){
+				for (int p = 0; p < b.taille; p++) {
+					if (carte[ligne][colone + p] != null) {
+						cut = true;
 					}
+					
 				}
-
+				}
+				
 				if (!cut) {
 					for (int j = 0; j < b.taille; j++) {
 						caseb.add(new CaseBateau(ligne, colone + j));
 					}
 					b.setEmplacement(caseb);
 					bateau.add(b);
-
+					
 					for (int k = 0; k < b.taille; k++) {
-
+					
 						carte[ligne][colone + k] = caseb.get(k);
-
+						
 					}
 					placer = true;
 				}
+				
+			
 
-				// Placement vertical
-			} else {
-
-				int ligne = 1 + (int) (Math.random() * (((carte.length - b.taille) - 1) + 1));
-				int colone = (int) 1 + (int) (Math.random() * ((carte.length - 2) + 1));
+			// Placement vertical
+		} else {
+			
+				int ligne = 0 + (int) (Math.random() * (((carte.length - b.taille) ) + 1));
+				int colone = (int) 0 + (int) (Math.random() * ((carte.length-1) + 1));
+				
 				ArrayList<CaseBateau> caseb = new ArrayList<CaseBateau>();
-
+				
 				for (int p = 0; p < b.taille; p++) {
-					if (ligne + p > carte.length - 1) {
+					if(ligne+p > carte.length-1){
 						System.out.println("yo");
 						cut = true;
 					}
 				}
-				if (!cut) {
+				if(!cut){
 					for (int p = 0; p < b.taille; p++) {
-						if (carte[ligne + p][colone] != null) {
-
-							cut = true;
-						}
+					if (carte[ligne + p][colone] != null) {
+					
+					cut = true;
 					}
+				}
 				}
 
 				if (!cut) {
@@ -112,10 +116,10 @@ public class Plateau {
 					}
 					b.setEmplacement(caseb);
 					bateau.add(b);
-
+					
 					for (int k = 0; k < b.getTaille(); k++) {
 						carte[ligne + k][colone] = caseb.get(k);
-
+						
 					}
 					placer = true;
 				}
@@ -123,6 +127,10 @@ public class Plateau {
 			}
 		}
 
+		
+		
+		
+		
 	}
 
 	public void ajouterBateau(Bateau b) {
--- src/Vue/VueJeu.java
+++ src/Vue/VueJeu.java
@@ -1,6 +1,5 @@
 package Vue;
 
-
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.EventQueue;
@@ -8,12 +7,14 @@ import java.awt.FlowLayout;
 import java.awt.GridBagLayout;
 
 import javax.swing.GroupLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SpringLayout;
 import javax.swing.border.EmptyBorder;
 
+import Controlleur.ControllerJeu;
 import Modele.CaseBateau;
 import Modele.Parametre;
 import Modele.Partie;
@@ -21,124 +22,179 @@ import Modele.Plateau;
 
 import javax.swing.JLabel;
 import java.awt.GridLayout;
+import java.awt.event.ActionListener;
 import java.util.Observable;
 import java.util.Observer;
 
-public class VueJeu extends JFrame implements Observer{
-	
-	//private JPanel panelInfo;
+public class VueJeu implements Observer {
+
+	private JFrame frame;
+	// private JPanel panelInfo;
 	private JTextField informations;
 	private JPanel contentPane;
 	private JPanel contentPane2;
-	private JTextField[][] Grille;
+	private JButton[][] Grille;
 	private JTextField[][] Grille2;
 	Alphabet tab = new Alphabet();
-	/**
-	 * Launch the application.
-	 */
-	
+	ControllerJeu cj = new ControllerJeu(this);
 
-	/**
-	 * Create the frame.
-	 */
+	public JFrame getFrame() {
+		return frame;
+	}
+
+	public void setFrame(JFrame frame) {
+		this.frame = frame;
+	}
+
+	public JTextField getInformations() {
+		return informations;
+	}
+
+	public void setInformations(JTextField informations) {
+		this.informations = informations;
+	}
+
+	public JPanel getContentPane() {
+		return contentPane;
+	}
+
+	public void setContentPane(JPanel contentPane) {
+		this.contentPane = contentPane;
+	}
+
+	public JPanel getContentPane2() {
+		return contentPane2;
+	}
+
+	public void setContentPane2(JPanel contentPane2) {
+		this.contentPane2 = contentPane2;
+	}
+
+	public JButton[][] getGrille() {
+		return Grille;
+	}
+
+	public void setGrille(JButton[][] grille) {
+		Grille = grille;
+	}
+
+	public JTextField[][] getGrille2() {
+		return Grille2;
+	}
+
+	public void setGrille2(JTextField[][] grille2) {
+		Grille2 = grille2;
+	}
+
+	public Alphabet getTab() {
+		return tab;
+	}
+
+	public void setTab(Alphabet tab) {
+		this.tab = tab;
+	}
+
+	public ControllerJeu getCj() {
+		return cj;
+	}
+
+	public void setCj(ControllerJeu cj) {
+		this.cj = cj;
+	}
+
 	public VueJeu() {
-		
-		getContentPane().setLayout(new GridLayout(3,1));
-		
+
+		frame = new JFrame();
+		frame.getContentPane().setLayout(new GridLayout(3, 1));
+
 		contentPane = new JPanel();
 		contentPane2 = new JPanel();
-		//panelInfo = new JPanel();
+		// panelInfo = new JPanel();
 		informations = new JTextField();
 		informations.setText("J'affiche les informations bitches");
-		
-		contentPane.setLayout(new GridLayout(Partie.getInstance().getParametres().getHauteurPlateau()+1,Partie.getInstance().getParametres().getHauteurPlateau()+1));
-		contentPane2.setLayout(new GridLayout(Partie.getInstance().getParametres().getHauteurPlateau()+1,Partie.getInstance().getParametres().getHauteurPlateau()+1));
-		//panelInfo.add(informations);
-		
-		this.setTitle("Jeu");
-		
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, (Partie.getInstance().getParametres().getHauteurPlateau()+1)*100, (Partie.getInstance().getParametres().getHauteurPlateau()+1)*100);
-		getContentPane().add(contentPane);
-		//informations.setBounds(informations.getX(), informations.getY(), 100, 100);
-		//informations.setMaximumSize(new Dimension(1100, 100));
-		
-		getContentPane().add(informations);
-		getContentPane().add(contentPane2);
-		Grille = new JTextField[Partie.getInstance().getParametres().getHauteurPlateau()+1][Partie.getInstance().getParametres().getLargeurPlateau()+1];
-		Grille2 = new JTextField[Partie.getInstance().getParametres().getLargeurPlateau()+1][Partie.getInstance().getParametres().getLargeurPlateau()+1];
-		
-		//Création de la grille de jeu
-		
 
-		
+		contentPane.setLayout(new GridLayout(Partie.getInstance().getParametres().getHauteurPlateau() + 1,
+				Partie.getInstance().getParametres().getHauteurPlateau() + 1));
+		contentPane2.setLayout(new GridLayout(Partie.getInstance().getParametres().getHauteurPlateau() + 1,
+				Partie.getInstance().getParametres().getHauteurPlateau() + 1));
 
-		
-		for(int i = 0; i<Grille.length; i++){
-			for(int k = 0; k<Grille.length; k++){
-				Grille[i][k] = new JTextField();
-				
-				Grille[i][k].setEditable(false);
-				if(i != 0 && k!= 0){
-		
-		
-		Grille[i][k].setText("");
-		
-		
-		
-		
-	
-		}else if ( i == 0 && k!= 0){
-			Grille[i][k].setText(Integer.toString(k));
-		}
+		frame.setTitle("Jeu");
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setBounds(100, 100, (Partie.getInstance().getParametres().getHauteurPlateau() + 1) * 100,
+				(Partie.getInstance().getParametres().getHauteurPlateau() + 1) * 100);
+		frame.getContentPane().add(contentPane);
+
+		frame.getContentPane().add(informations);
+		frame.getContentPane().add(contentPane2);
+		Grille = new JButton[Partie.getInstance().getParametres().getHauteurPlateau()
+				+ 1][Partie.getInstance().getParametres().getLargeurPlateau() + 1];
+		Grille2 = new JTextField[Partie.getInstance().getParametres().getLargeurPlateau()
+				+ 1][Partie.getInstance().getParametres().getLargeurPlateau() + 1];
+
+		// Création de la grille de jeu
+
+		for (int i = 0; i < Grille.length; i++) {
+			for (int k = 0; k < Grille.length; k++) {
+				Grille[i][k] = new JButton();
+				Grille[i][k].addMouseListener(cj);
+
+				if (i != 0 && k != 0) {
+
+					Grille[i][k].setText("");
+
+				} else if (i == 0 && k != 0) {
+					Grille[i][k].setText(Integer.toString(k));
+				}
 				contentPane.add(Grille[i][k]);
+			}
+
 		}
-			
-		}
-		for(int j = 0; j<Grille2.length; j++){
-			for(int l = 0; l<Grille2.length; l++){
+
+		for (int j = 0; j < Grille2.length; j++) {
+			for (int l = 0; l < Grille2.length; l++) {
 				Grille2[j][l] = new JTextField();
 				Grille2[j][l].setEditable(false);
-				if(j != 0 && l!= 0){
-		
-		
-		Grille2[j][l].setText("");
-		
-		
-		
-		
-	
-		}else if ( j == 0 && l!= 0){
-			Grille2[j][l].setText(Integer.toString(l));
-		}
+
+				if (j != 0 && l != 0) {
+					System.out.println(j + " " + l + "" + Grille2.length);
+					if (!(j == Grille2.length-1) && !(l == Grille2.length -1)) {
+						if (Partie.getInstance().getJoueur().getCarte()[j][l] instanceof CaseBateau) {
+							
+							Grille2[j][l].setBackground(Color.RED);
+							
+						}
+					}
+					Grille2[j][l].setText("");
+
+				} else if (j == 0 && l != 0) {
+					Grille2[j][l].setText(Integer.toString(l));
+				}
 				contentPane2.add(Grille2[j][l]);
+			}
+
 		}
-			
-		}
-		
-	
-		for(int a = 1; a<Grille.length;a++){
-			
+
+		for (int a = 1; a < Grille.length; a++) {
+
 			Grille[a][0].setText(tab.getAlphabet()[a]);
 			Grille2[a][0].setText(tab.getAlphabet()[a]);
 		}
-		
-		
-	}
 
+	}
 
 	public void update(Observable o, Object arg) {
 		// TODO Auto-generated method stub
-		/*if(arg instanceof Plateau){
-		for(int i = 0; i < Grille.length; i++){
-			for(int j = 0; j< Grille.length;i++){
-				if(((Plateau) arg).getCarte()[i][j] instanceof CaseBateau){
-					Grille[i][j].setBackground(Color.RED);
-					
+		if (arg instanceof Plateau) {
+			for (int i = 0; i < Grille2.length; i++) {
+				for (int j = 0; j < Grille2.length; i++) {
+					if (((Plateau) arg).getCarte()[i][j] instanceof CaseBateau) {
+						Grille2[i][j].setBackground(Color.RED);
+
+					}
 				}
 			}
 		}
-		}*/
-		
+
 	}
 }
